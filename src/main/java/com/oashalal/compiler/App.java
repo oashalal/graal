51502f1e0466491ddebc5a9c46ef2cf7d60b22b1
oashalal <ssviridov829@gmail.com>
2024-08-22 12:34:16 +0300

    init

---
+++ src/main/java/com/oashalal/compiler/App.java
@@ -0,0 +1,15 @@
+package com.oashalal.compiler;
+
+/**
+ * Hello world!
+ *
+ */
+public class App {
+
+    /**
+     * @see this is main Method
+     */
+    public static void main( String[] args ) {
+        System.out.println("Hello World!");
+    }
+}
